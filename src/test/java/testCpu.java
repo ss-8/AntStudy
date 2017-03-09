c09eae307f361484eb8be0037cbeb13081092c27
bjbianwenxing <bianwenxing@jd.com>
2017-03-09 18:03:50 +0800

    aa

--- src/test/java/testCpu.java
+++ src/test/java/testCpu.java
@@ -6,7 +6,10 @@ public class testCpu {
 
 	@Test
 	public void test() {
-		System.out.println(Runtime.getRuntime().availableProcessors());
+		//System.out.println(Runtime.getRuntime().availableProcessors());
+		long s=System.currentTimeMillis();
+		System.out.println(Math.sqrt(20));
+		System.out.println(System.currentTimeMillis()-s);
 	}
 
 }
